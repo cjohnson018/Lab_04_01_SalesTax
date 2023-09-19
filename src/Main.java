2a24011be9a449520ee5cca683e9fff6c392c8fb
cjohnson018 <fun2bcd@gmail.com>
2023-09-18 20:14:38 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        double purchasePrice = 30;
+        double salesTax = .05 * purchasePrice;
+        System.out.println("Your initial cost was " + purchasePrice + " the tax on it is " + salesTax);
+    }
+}
